290f769dd89f798fb76a8d942f689094ee7be615
KawfeyShop <logancemail@gmail.com>
2025-01-21 21:22:01 -0500

    added website prototype

---
+++ Projects/Login Prototype/server.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = json.loads(post_data)
+
+            username = data.get('username')
+            password = data.get('password')
+
+            print(f"Received Username: {username}, Password: {password}")
+
+            # Perform authentication logic here
+            if username == "admin" and password == "password":
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b'{"message": "Login successful"}')
+            else:
+                self.send_response(401)
+                self.end_headers()
+                self.wfile.write(b'{"message": "Invalid credentials"}')
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ('127.0.0.1', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running on http://127.0.0.1:{port}")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
